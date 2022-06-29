376e5b74741d8cd7e4505f7eed1af453375319e7
Ana <amarcosestevez@gmail.com>
2022-06-29 12:37:58 +0200

    bootstrap dependencies added

--- src/components/container/task_list.jsx
+++ src/components/container/task_list.jsx
@@ -4,8 +4,13 @@ import {  Task } from '../../models/task.class';
 import TaskComponent from '../pure/task';
 
 const TaskListComponent = () => {
+    const defaultTask1 = new Task('Example1', 'Description1', true, LEVELS.NORMAL);
 
-    const defaultTask = new Task('Example', 'Default description', false, LEVELS.NORMAL);
+    const defaultTask2 = new Task('Example2', 'Description2', false, LEVELS.URGENT);
+    
+    const defaultTask3 = new Task('Example3', 'Description3', false, LEVELS.BLOCKING);
+    
+    const [tasks, settasks] = useState(defaultTask1,defaultTask2,defaultTask3);
 
     return (
         <div>
@@ -13,7 +18,7 @@ const TaskListComponent = () => {
                 <h1> Your Tasks: </h1>
             </div>
             {/* To do: Aplicar un map o un for para renderizar una lista*/}
-            <TaskComponent task={defaultTask} />
+            <TaskComponent task={tasks} />
         </div>
     );
 };
