31112334cec80b68184a25b09afb5e574b2b5749
Ana <amarcosestevez@gmail.com>
2022-07-06 14:01:25 +0200

    add conditional render at task-list component

--- src/components/container/task_list.jsx
+++ src/components/container/task_list.jsx
@@ -39,11 +39,49 @@ function handleDelete(task){
      setTasks(tempTask)
 }
 function addTask(task){
-    const index = tasks.indexOf(task);
     const tempTask = [...tasks];
    tempTask.push(task);
      setTasks(tempTask)
 }
+    const Table = () => {
+        return(
+        <table>
+            <thead>
+                <tr>
+                    <th scope='col'>
+                        Title
+                    </th>
+                    <th scope='col'>
+                        Description
+                    </th>
+                    <th scope='col'>
+                        Priority
+                    </th>
+                    <th scope='col'>
+                        Actions
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                {tasks.map((task, index) => {
+                    return (<TaskComponent key={index} task={task} handleCompleteTask={handleCompleteTask} handleDelete={handleDelete} />)
+                })}
+               
+
+            </tbody>
+        </table>)
+    }
+    let taskTable
+    if (tasks.length > 0) {
+        taskTable = <Table />
+    } else {
+        taskTable =
+            <div>
+                <h3>There are no tasks to show</h3>
+                <h4>Please create one</h4>
+            </div>
+
+    }
     return (
         <div>
             <div className='col-12'>
@@ -55,32 +93,9 @@ function addTask(task){
                         </h5>
                     </div>
                     {/*Card body:content*/ }
-                    <div className='card-body' data-mdb-perfect-scrollbar='true' style={{ position: 'relative', height: '400px' }}>
-                        <table>
-                            <thead>
-                                <tr>
-                                    <th scope='col'>
-                                        Title
-                                    </th>
-                                    <th scope='col'>
-                                        Description
-                                    </th>
-                                    <th scope='col'>
-                                        Priority
-                                    </th>
-                                    <th scope='col'>
-                                        Actions
-                                    </th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {tasks.map((task, index) => {
-                                    return (<TaskComponent key={index} task={task} handleCompleteTask={handleCompleteTask} handleDelete={handleDelete}/>)
-                                })}
-<TaskForm add={addTask} />
-
-                            </tbody>
-                        </table>
+                    <div className='card-body' data-mdb-perfect-scrollbar='true' style={{ position: 'relative' }}>
+                       {taskTable}
+                       <TaskForm add={addTask} />
                     </div>
                 </div>
 
---
+++ src/pages/tasks/TaskaPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TaskListComponent from '../../components/container/task_list';
+
+const TaskaPage = () => {
+    return (
+        <div>
+            <TaskListComponent/>
+        </div>
+    );
+}
+
+export default TaskaPage;
