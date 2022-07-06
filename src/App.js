97533b07c02194e10596589735631eb35fc09980
Ana <amarcosestevez@gmail.com>
2022-07-06 19:15:48 +0200

    install formik and yup

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import '././styles/App.css';
 //import GreetingF from './components/pure/greetingF';
 import TaskListComponent from './components/container/task_list';
 import React from 'react';
+import LoginFormik from './components/pure/forms/LoginFormik';
 //import ContactListComponent from './components/container/contact_list';
 //import { useState } from 'react';
 function App() {
   return (
     <div className="App">
+    <LoginFormik/>
    <TaskListComponent/>
     </div>
   );
---
+++ src/components/pure/forms/LoginFormik.jsx
@@ -0,0 +1,89 @@
+import React from 'react';
+import { Formik, Field, Form } from 'formik';
+import * as Yup from 'yup';
+const loginSchema = Yup.object().shape(
+    {
+        email:Yup.string().email('Invalid email format').required('Email is required'),
+        password: Yup.string().required('Password is required')
+    }
+)
+const LoginFormik = () => {
+    const initialCredentials = {
+        email:'', 
+        password:''
+    }
+    return (
+        <div>
+    <h4>Login Formik</h4>
+        <Formik
+        // Initial values that the form will take
+
+        initialValues={initialCredentials }
+
+        // Yup validation schema
+
+        validationSchema = {loginSchema}
+
+        //onsubmit event
+
+        onSubmit={ async (values) => {
+        await new Promise((r) => setTimeout(r, 500));
+        alert(JSON.stringify(values, null, 2));
+         }} 
+         >
+
+
+         {/* We obtain props from Formik*/}
+         { props =>{
+            const {
+                values,
+                touched,
+                errors,
+                isSubmitting,
+                handleChange,
+                handleBlur,
+    
+           }= props;
+         return( 
+           <Form>
+        <label htmlFor="email">Email</label>
+        <Field
+          id="email"
+          name="email"
+          placeholder="jane@acme.com"
+          type="email"
+        />
+        {/*Email errors */ }
+        {
+            errors.email && touched.email && (
+                <div className='error'>
+                    <p>
+                        {errors.email}
+                    </p>
+                </div>
+            )
+        }
+<label htmlFor="password">Password</label>
+        <Field id="password" name="password"   placeholder="password"
+          type="password" />
+ {/*password errors */ }
+ {
+            errors.password && touched.password && (
+                <div className='error'>
+                    <p>
+                        {errors.password}
+                    </p>
+                </div>
+            )
+        }
+
+        <button type="submit">Submit</button>
+</Form> 
+         )
+         }}
+        </Formik>
+        </div>
+    );
+}
+
+export default LoginFormik;
