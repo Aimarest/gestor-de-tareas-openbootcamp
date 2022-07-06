e719f5b87a042849aca8cf3186527c3e3036a85a
Ana <amarcosestevez@gmail.com>
2022-07-06 19:37:55 +0200

    refactory error message of password

--- src/components/pure/forms/LoginFormik.jsx
+++ src/components/pure/forms/LoginFormik.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Formik, Field, Form } from 'formik';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 const loginSchema = Yup.object().shape(
     {
@@ -49,28 +49,33 @@ const LoginFormik = () => {
           placeholder="jane@acme.com"
           type="email"
         />
-        {/*Email errors */ }
+        {/*Email errors. Hay 2 formas de hacerlo,haciendolo nosotros mismos o  utilizando el errorMessage de formik de ejemplo*/ }
         {
-            errors.email && touched.email && (
+           /*} errors.email && touched.email && (
                 <div className='error'>
                     <p>
                         {errors.email}
                     </p>
-                </div>
-            )
+                </div>*/
+                <ErrorMessage name="email" component='div'/>
+                
+            
         }
 <label htmlFor="password">Password</label>
         <Field id="password" name="password"   placeholder="password"
           type="password" />
  {/*password errors */ }
  {
-            errors.password && touched.password && (
+           /* errors.password && touched.password && (
                 <div className='error'>
                     <p>
                         {errors.password}
                     </p>
                 </div>
             )
+            */
+
+            <ErrorMessage name="password" component='div'/>
         }
 
         <button type="submit">Submit</button>
