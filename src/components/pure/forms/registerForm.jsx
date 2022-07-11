3b47bcbdc62207807df4ec2f0c0e141dabe91426
Ana <amarcosestevez@gmail.com>
2022-07-11 18:27:55 +0200

    add confirm password at form with Yup

--- src/components/pure/forms/registerForm.jsx
+++ src/components/pure/forms/registerForm.jsx
@@ -1,14 +1,45 @@
 import React, {useState} from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+
+//Models:
+import { User } from '../../../models/user.class';
+import { ROLES } from '../../../models/roles.enum';
 
 const RegisterForm = () => {
-    const initialData = [
-        { 
-           user: '',
-           name:'',
-           email:'',
-          password: ''}
-      ];
-      const [data, setData] = useState(initialData);
+
+   let user = new User();
+
+const initialValues = {
+
+    username: '',
+ email:'',
+ password: '',
+ confirm:'',
+ role:ROLES.USER
+}
+
+const registerSchema = Yup.object().shape(
+username: Yup.string()
+.min(6,'username too short')
+.max(12,'username too long')
+.required('username is required'),
+email:Yup.string().email('Invalid email format').required('Email is required'),
+password: Yup.string().required('Password is required').min(8,'Password too short'),
+confirm: Yup.string().when('password',{
+    is:value => (value && value.length > 0 ? true : false),
+    then: Yup.string().oneOf(
+        [Yup.ref('password')],
+        '¡Password must match!'
+    )
+}),required('You must confirm the password')
+)
+
+
+const submit = (values)=>{
+    alert('Register user')
+}
+    
     return (
         <div>
             
