ff128a6a6e6bf324270330997c444543a932e0c3
Ana <amarcosestevez@gmail.com>
2022-07-11 18:59:40 +0200

    finish register form with formik

--- src/components/pure/forms/registerForm.jsx
+++ src/components/pure/forms/registerForm.jsx
@@ -32,7 +32,8 @@ confirm: Yup.string().when('password',{
         [Yup.ref('password')],
         '¡Password must match!'
     )
-}),required('You must confirm the password')
+}),required('You must confirm the password'),
+role: Yup.string().oneOf(ROLES.USER, ROLES.ADMIN, 'You must select a Role: User / Admin').required('Role is required')
 )
 
 
@@ -42,7 +43,76 @@ const submit = (values)=>{
     
     return (
         <div>
-            
+          <h4>Register Formik</h4> 
+          <Formik initialValues={initialValues}
+
+
+            //onsubmit event
+
+        onSubmit={ async (values) => {
+        await new Promise((response) => setTimeout(response, 1000));
+        alert(JSON.stringify(values, null, 2));
+        
+        // Yup validation schema
+
+        validationSchema = {registerSchema}
+         }} 
+        >
+             {/* We obtain props from Formik*/}
+    
+             {({errors,values, touched,isSubmitting, handleBlur,handleChange}) => (
+        
+        <Form>
+        <label htmlFor="username">Username</label>
+     <Field
+       id="username"
+       name="username"
+       placeholder="Your username"
+       type="text"
+     />
+      {/*Username errors */}
+       
+     { errors.username && touched.username && (
+        <ErrorMessage name = 'username ' component = 'div'></ErrorMessage>
+     )}
+     <label htmlFor="email">Email</label>
+     <Field
+       id="email"
+       name="email"
+       placeholder="example@email.com"
+       type="email"
+     />
+     {/*Email errors*/ }
+     {
+        
+             <ErrorMessage name="email" component='div'/>
+             
+         
+     }
+<label htmlFor="password">Password</label>
+     <Field id="password" name="password"   placeholder="password"
+       type="password" />
+{/*password errors */ }
+                        {
+
+                            <ErrorMessage name="password" component='div' />
+                        }
+     <label htmlFor="confirm">Confirm password</label>
+     <Field id="confirm" name="confirm"   placeholder="Confirm your password"
+       type="password" />
+{/*Confirm password errors */ }
+   { 
+    errors.confirm && touched.confirm && 
+   ( <ErrorMessage name="confirm " component='div'/> )}
+
+   <label htmlFor="role">role</label>
+    
+     <button type="submit">Submit</button>
+
+     {isSubmitting ? (<p>Saving your credentials...</p>) : null}
+</Form> )};
+
+        </Formik>
         </div>
     );
 }
